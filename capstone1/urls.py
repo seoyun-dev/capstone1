70a3ea115f00488e0f1804962884c8262d160bfc
seoyun <dhn04100@gmail.com>
2023-04-28 22:51:02 +0900

    REFACTORING urls.py for mainpage

--- capstone1/urls.py
+++ capstone1/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     # path('carts', include('carts.urls')),
     # path('orders', include('orders.urls')),
-    path('products', include('products.urls')),
+    path('', include('products.urls')),
     # path('users', include('users.urls')),
 ]
 
--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from products.views import ProductListView, ProductDetailView
 
 urlpatterns = [
+    path('products', ProductListView.as_view()),
     path('', ProductListView.as_view()),
-    path('/<int:product_id>', ProductDetailView.as_view()),
+    path('products/<int:product_id>', ProductDetailView.as_view()),
 ]
