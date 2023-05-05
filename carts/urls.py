4e438779a702415e38b47d549fe102deeb8bed73
seoyun <dhn04100@gmail.com>
2023-05-05 17:07:27 +0900

    REFACTORING remove carts codes

--- carts/urls.py
+++ carts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from carts.views import CartView
+# from carts.views import CartView
 
 urlpatterns = [
-    path('', CartView.as_view())
+    # path('', CartView.as_view())
 ]
